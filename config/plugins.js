cdb5db9dc540ebe10b9e47ae2122dbb98619c88b
Alex <alex@roses.email>
2025-06-04 13:31:23 +0100

    Cache enabled

--- config/plugins.js
+++ config/plugins.js
@@ -1,1 +1,20 @@
-module.exports = () => ({});
+module.exports = () => ({
+
+// config/plugins.{js,ts}
+'strapi-cache': {
+    enabled: true,
+    config: {
+      debug: false, // Enable debug logs
+      max: 1000, // Maximum number of items in the cache (only for memory cache)
+      ttl: 1000 * 60 * 60, // Time to live for cache items (1 hour)
+      size: 1024 * 1024 * 1024, // Maximum size of the cache (1 GB) (only for memory cache)
+      allowStale: false, // Allow stale cache items (only for memory cache)
+      cacheableRoutes: [''], // Caches routes which start with these paths (if empty array, all '/api' routes are cached)
+      provider: 'memory', // Cache provider ('memory' or 'redis')
+      cacheHeaders: true, // Plugin also stores response headers in the cache (set to false if you don't want to cache headers)
+      cacheAuthorizedRequests: false, // Cache requests with authorization headers (set to true if you want to cache authorized requests)
+      cacheGetTimeoutInMs: 1000, // Timeout for getting cached data in milliseconds (default is 1 seconds)
+    },
+  }
+  
+});
