e75444b4a9955a36008e25b5fc6da667dc6bfcfd
Alex <alex@roses.email>
2025-06-05 14:41:09 +0100

    s3 updates

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,7 +1,32 @@
 module.exports = [
   'strapi::logger',
   'strapi::errors',
-  'strapi::security',
+  {
+    name: 'strapi::security',
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          'connect-src': ["'self'", 'https:'],
+          'img-src': [
+            "'self'",
+            'data:',
+            'blob:',
+            'dl.airtable.com',
+            '',
+          ],
+          'media-src': [
+            "'self'",
+            'data:',
+            'blob:',
+            'dl.airtable.com',
+            'strapi-p-storage.s3.us-west-2.amazonaws.com',
+          ],
+          upgradeInsecureRequests: null,
+        },
+      },
+    },
+  },
   'strapi::cors',
   'strapi::poweredBy',
   'strapi::query',
--- config/plugins.js
+++ config/plugins.js
@@ -3,27 +3,24 @@ module.exports = ({ env }) => ({
         enabled: true,
         },
         upload: {
-            config: {
-              provider: 'aws-s3',
-              providerOptions: {
-                s3Options: {
-                  accessKeyId: env('AWS_ACCESS_KEY_ID'),
-                  secretAccessKey: env('AWS_ACCESS_SECRET'),
-                  region: env('AWS_REGION'),
-                  params: {
-                    Bucket: env('AWS_BUCKET_NAME'),
-                  },
-                }
+          config: {
+            provider: 'aws-s3',
+            providerOptions: {
+              accessKeyId: env('AWS_ACCESS_KEY_ID'),
+              secretAccessKey: env('AWS_ACCESS_SECRET'),
+              region: env('AWS_REGION'),
+              params: {
+                ACL: env('AWS_ACL', 'public-read'),
+                signedUrlExpires: env('AWS_SIGNED_URL_EXPIRES', 15 * 60),
+                Bucket: env('AWS_BUCKET'),
               },
-              actionOptions: {
-                upload: {
-                  ACL: null
-                },
-                uploadStream: {
-                  ACL: null
-                },
-              }
             },
+            actionOptions: {
+              upload: {},
+              uploadStream: {},
+              delete: {},
+            },
+          },
         },
     // config/plugins.{js,ts}
     'strapi-cache': {
